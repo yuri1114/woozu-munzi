0ed81b97f1e38ed3e533e7bdbf1ad1e99297f344
yuri <jyuri1114@naver.com>
2026-02-26 23:35:11 +0900

    메모장 퍼블리싱

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-$(function () {});
+$(function () {
+
+    $('#write-popup').hide();
+    $('#start-btn').on('click', function(){
+        $('#write-popup').show();
+    })
+
+    $('#close-btn').on('click', function(){
+        if(confirm('먼지를 남기지 않으시겠습니까? 작성된먼지가 삭제됩니다')){
+            $('#write-popup').hide();
+            $('#textareaMunze').val('');
+        }else{
+            //TODO bye하며 사라지는 애니메이션
+        }
+    })
+
+    $('#save-btn').on('click', function(){
+        //저장로직
+    })
+});
